5859b85752bc8d86f85a262239acd16b3adf3692
Daniel Angell <me@danangell.com>
2023-12-03 09:00:41 -0800

    Support v2 and v3 manifests

--- build.sh
+++ build.sh
@@ -2,4 +2,11 @@
 
 # Add all files in src/* to a zip called extension.xpi.
 cd "$(dirname "$0")"/src
-zip -r ../extension.xpi *
+
+cp ../manifests/v2.json ./manifest.json
+zip -r ../out/extension.xpi *
+
+cp ../manifests/v3.json ./manifest.json
+zip -r ../out/extension.crx *
+
+rm ./manifest.json
